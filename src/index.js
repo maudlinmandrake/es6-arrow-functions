552c83d7e11f4b3b15bba785de8c134e07e212bd
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-14 16:44:34 -0400

    Challenge: rewrite reduce() using fat arrow

--- src/index.js
+++ src/index.js
@@ -15,19 +15,24 @@ var numbers = [3, 56, 2, 48, 5];
 // console.log(newNumbers);
 
 ////Map -Create a new array by doing something with each item in an array.
-const newNumbers = numbers.map((x) => x * 2);
 
-console.log(newNumbers);
+// const newNumbers = numbers.map((x) => x * 2);
+
+// console.log(newNumbers);
 
 //////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+
+// const newNumbers = numbers.filter( (num) => num < 10 );
+
+// console.log(newNumbers);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+
+var newNumber = numbers.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
+
+console.log(newNumber);
 
 ////Find - find the first item that matches from an array.
 // const newNumber = numbers.find(function (num) {
